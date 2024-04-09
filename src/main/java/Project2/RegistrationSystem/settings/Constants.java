8a0117bbd0101c82cc059c5da501d7ff8d8a5264
Jiri Loukota <jloukota@seznam.cz>
2024-04-09 07:13:02 +0200

    Removed personal login info for database.

--- src/main/java/Project2/RegistrationSystem/settings/Constants.java
+++ src/main/java/Project2/RegistrationSystem/settings/Constants.java
@@ -3,6 +3,6 @@ package Project2.RegistrationSystem.settings;
 public class Constants {
     public static final String PERSON_ID_FILE_PATH = "files/dataPersonId.txt",
             DATABASE_URL = "jdbc:mysql://localhost:3306/usersdatabase",
-            DATABASE_USER_NAME = "myUser",
-            DATABASE_PASSWORD = "f#17GHkl0@98mn3/ilk2#";
+            DATABASE_USER_NAME = "",
+            DATABASE_PASSWORD = "";
 }
